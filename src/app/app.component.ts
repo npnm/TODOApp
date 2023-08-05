b9044512bc8e450ce2728aeaad9c71f11106d0f2
npnm <nupindev@gmail.com>
2023-08-05 12:04:34 +0530

    update dashboard to generate todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'TODOApp1';
+  public title = 'TODOApp';
+  public todoList: Array<string> = [
+    'Item1',
+    'Item2',
+    'Item3',
+    'Item4',
+    'Item5',
+    'Item6',
+    'Item7',
+    'Item8',
+  ]
+  constructor() {
+
+  }
 }
